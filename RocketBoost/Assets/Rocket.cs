0ec059893df06525377437619ec15d379c3d16d9
jasonoh1226 <jasonoh1226@gmail.com>
2020-02-19 11:06:55 -0500

    Add natural movement

---
+++ RocketBoost/Assets/Rocket.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Member variables
+    [SerializeField] float controlThrust = 100f;
+    Rigidbody rigidbody;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+        // Fetch the AudioSource from the GameObject
+        audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
+        // Can thrust while rotating
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+
+            // So it doesn't layer each other
+            if (!audioSource.isPlaying)
+                audioSource.Play();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        // Take manual control of rotation
+        rigidbody.freezeRotation = true;
+
+        float rotationThisFrame = controlThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        // Resume physics control of rotation
+        rigidbody.freezeRotation = false;
+    }
+
+    
+}
